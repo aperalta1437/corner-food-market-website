679e4828874f989e97b1962f76e975658ab9d154
Angel Peralta <amiguelp007@gmail.com>
2021-06-09 18:23:50 -0400

    First commit

---
+++ corner-food-market-website/src/main/java/com/psu/ist311/team/team5/cornerfoodmarketwebsite/CornerFoodMarketWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.psu.ist311.team.team5.cornerfoodmarketwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerFoodMarketWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornerFoodMarketWebsiteApplication.class, args);
+	}
+
+}
