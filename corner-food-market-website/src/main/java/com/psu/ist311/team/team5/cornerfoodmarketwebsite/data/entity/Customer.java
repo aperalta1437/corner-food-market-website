5d40fc75bdca263530fb78c2d10f3c531bbf462a
Angel Peralta <amiguelp007@gmail.com>
2021-06-19 12:02:22 -0400

    Created Customer entity and repository as well as JUnit Customer Creation test.

---
+++ corner-food-market-website/src/main/java/com/psu/ist311/team/team5/cornerfoodmarketwebsite/data/entity/Customer.java
@@ -0,0 +1,70 @@
+package com.psu.ist311.team.team5.cornerfoodmarketwebsite.data.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CUSTOMER")
+public class Customer {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short id;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "CELL_PHONE_NUMBER")
+    private String cellPhoneNumber;
+
+    public short getId() {
+        return id;
+    }
+
+    public void setId(short id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCellPhoneNumber() {
+        return cellPhoneNumber;
+    }
+
+    public void setCellPhoneNumber(String cellPhoneNumber) {
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+}
